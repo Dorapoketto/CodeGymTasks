658da3f2e2b27a07db3104463f3d2a453dddb89c
Dorapoketto <mxc2012@outlook.com>
2021-03-10 17:33:40 +0800

    Add: 10818

--- 1.JavaSyntax/src/zh/codegym/task/task08/task0818/Solution.java
+++ 1.JavaSyntax/src/zh/codegym/task/task08/task0818/Solution.java
@@ -9,11 +9,26 @@ import java.util.Map;
 
 public class Solution {
     public static HashMap<String, Integer> createMap() {
-        //在此编写你的代码
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Stallone", 500);
+        map.put("Stallone2", 300);
+        map.put("Stallone3", 100);
+        map.put("Schwarzenegger", 2000);
+        map.put("Schwarzenegger2", 200);
+        map.put("Schwarzenegger3", 600);
+        map.put("Willis", 700);
+        map.put("Willis2", 850);
+        map.put("Willis3", 725);
+        map.put("Stallone4", 530);
+        return map;
     }
 
     public static void removeItemFromMap(HashMap<String, Integer> map) {
-        //在此编写你的代码
+        HashMap<String, Integer> copy = new HashMap<>(map);
+        for (Map.Entry<String, Integer> pair : copy.entrySet()) {
+            if (pair.getValue() < 500)
+                map.remove(pair.getKey());
+        }
     }
 
     public static void main(String[] args) {
