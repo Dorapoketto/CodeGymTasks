520275968459e17a479545e5bcb3b5162eb116eb
Dorapoketto <mxc2012@outlook.com>
2021-03-10 17:33:50 +0800

    Add: 10442

---
+++ 1.JavaSyntax/src/zh/codegym/task/task04/task0442/Solution.java
@@ -0,0 +1,14 @@
+package zh.codegym.task.task04.task0442;
+
+
+/* 
+相加
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //在此编写你的代码
+    }
+}
