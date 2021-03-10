94d2c9e9d3a43d5dcf69e90c6e540d458bc9f88b
Dorapoketto <mxc2012@outlook.com>
2021-03-10 20:12:00 +0800

    Add: 10442

--- 1.JavaSyntax/src/zh/codegym/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/zh/codegym/task/task04/task0442/Solution.java
@@ -6,9 +6,22 @@ package zh.codegym.task.task04.task0442;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //在此编写你的代码
+        Scanner scanner = new Scanner(System.in);
+        int sum  = 0;
+
+        while (true){
+            int number = scanner.nextInt();
+            if (number == -1){
+                sum = sum - 1;
+                break;
+            }
+            sum += number;
+        }
+        System.out.println(sum);
     }
 }
