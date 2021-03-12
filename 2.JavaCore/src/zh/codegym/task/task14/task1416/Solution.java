04114cae3165a258b248d877424599283b7dbf0c
Dorapoketto <mxc2012@outlook.com>
2021-03-12 15:54:58 +0800

    Add: 21416

---
+++ 2.JavaCore/src/zh/codegym/task/task14/task1416/Solution.java
@@ -0,0 +1,69 @@
+package zh.codegym.task.task14.task1416;
+
+/* 
+修复错误
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        CanSwim creature = new Orca();
+        creature.swim();
+        creature = new Whale();
+        creature.swim();
+        creature = new RiverOtter();
+        creature.swim();
+    }
+
+    public static void test(CanSwim creature) {
+        creature.swim();
+    }
+
+    interface CanWalk {
+        void walk();
+    }
+
+    interface CanSwim {
+        void swim();
+    }
+
+    static abstract class SeaCreature{
+        public void swim() {
+            SeaCreature currentCreature = (SeaCreature) getCurrentCreature();
+            currentCreature.displaySwim();
+        }
+
+        private void displaySwim() {
+            System.out.println(getCurrentCreature().getClass().getSimpleName() + " 正在游泳");
+        }
+
+        abstract CanSwim getCurrentCreature();
+    }
+
+    static class Orca extends SeaCreature implements CanSwim{
+        @Override
+        CanSwim getCurrentCreature() {
+            return this;
+        }
+    }
+
+    static class Whale extends SeaCreature implements CanSwim{
+
+        @Override
+        CanSwim getCurrentCreature() {
+            return this;
+        }
+    }
+
+    static class RiverOtter implements CanSwim, CanWalk{
+
+        @Override
+        public void walk() {
+
+        }
+
+        @Override
+        public void swim() {
+
+        }
+    }
+}
