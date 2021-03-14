fc3bd6ae1f52e68120fe8d9f502de52d21702db2
Dorapoketto <mxc2012@outlook.com>
2021-03-14 16:51:38 +0800

    Add: 21328

---
+++ 2.JavaCore/src/zh/codegym/task/task13/task1328/Robot.java
@@ -0,0 +1,16 @@
+package zh.codegym.task.task13.task1328;
+
+public class Robot extends AbstractRobot implements CanAttack, CanDefend {
+
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
