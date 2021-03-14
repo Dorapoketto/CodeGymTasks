5acd7c9ce2432e992b2484cd072a7d0703bd929f
Dorapoketto <mxc2012@outlook.com>
2021-03-14 16:51:48 +0800

    Add: 21411

---
+++ 2.JavaCore/src/zh/codegym/task/task14/task1411/Solution.java
@@ -0,0 +1,29 @@
+package zh.codegym.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User、loser、coder 和 programmer
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        // 这是读取键的循环。项目 1
+        {
+            // 创建一个对象。项目 2
+
+            doWork(person); // 调用 doWork
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        // 项目 3
+    }
+}
