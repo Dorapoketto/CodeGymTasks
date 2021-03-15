aef236a03efe7168795f5b1b3f28b0a3f66c5817
Dorapoketto <mxc2012@outlook.com>
2021-03-15 16:22:18 +0800

    Add: 21518

---
+++ 2.JavaCore/src/zh/codegym/task/task15/task1518/Solution.java
@@ -0,0 +1,25 @@
+package zh.codegym.task.task15.task1518;
+
+/* 
+Static 修饰符和小猫
+*/
+
+public class Solution {
+    public static Cat cat;
+
+    static {
+        cat  = new Cat();
+        cat.name = "hhh";
+        System.out.println(cat.name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Cat{
+        public String name;
+
+
+    }
+}
