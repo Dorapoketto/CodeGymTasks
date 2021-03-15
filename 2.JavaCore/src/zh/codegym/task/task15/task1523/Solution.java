b6896df47ddb13310383d8883ec9b333799d89ac
Dorapoketto <mxc2012@outlook.com>
2021-03-15 19:14:05 +0800

    Add: 21523

---
+++ 2.JavaCore/src/zh/codegym/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package zh.codegym.task.task15.task1523;
+
+/* 
+重载构造方法
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+
+    }
+
+    private Solution(int i) {
+
+    }
+
+    protected Solution(double i) {
+
+    }
+
+    Solution(short i) {
+
+    }
+}
+
