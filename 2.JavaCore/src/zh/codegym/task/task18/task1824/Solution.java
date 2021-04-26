564ee734596ca1024ee930bf6088ceb75f899e94
Dorapoketto <mxc2012@outlook.com>
2021-04-26 16:15:27 +0800

    Add: 21824

---
+++ 2.JavaCore/src/zh/codegym/task/task18/task1824/Solution.java
@@ -0,0 +1,24 @@
+package zh.codegym.task.task18.task1824;
+
+/* 
+文件和异常
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String fileName = reader.readLine();
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                break;
+            }
+        }
+
+    }
+}
