66dd3c6a70e595686b7cdb9e6fbf545344b869eb
Dorapoketto <mxc2012@outlook.com>
2021-05-08 10:23:57 +0800

    Add: 21820

---
+++ 2.JavaCore/src/zh/codegym/task/task18/task1820/Solution.java
@@ -0,0 +1,31 @@
+package zh.codegym.task.task18.task1820;
+
+/* 
+数字四舍五入
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName1 = bufferedReader.readLine();
+        String fileName2 = bufferedReader.readLine();
+
+
+
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(fileName1));
+             PrintWriter printWriter = new PrintWriter(new FileWriter(fileName2))) {
+
+            while (bufferedFileReader.ready()) {
+                String[] splittedLine = bufferedFileReader.readLine().split(" ");
+                for (String numberInString : splittedLine) {
+                    double number = Double.parseDouble(numberInString);
+                    long roundedNumber = Math.round(number);
+                    printWriter.print(roundedNumber + " ");
+                }
+            }
+        }
+    }
+}
