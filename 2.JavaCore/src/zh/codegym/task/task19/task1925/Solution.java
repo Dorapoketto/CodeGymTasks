a1a122d8b7c4e7425cedb37411091a7fb992d6b2
Dorapoketto <mxc2012@outlook.com>
2021-06-07 17:20:30 +0800

    Add: 21925

---
+++ 2.JavaCore/src/zh/codegym/task/task19/task1925/Solution.java
@@ -0,0 +1,46 @@
+package zh.codegym.task.task19.task1925;
+
+/* 
+长字词
+*/
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String fileName1 = args[0];
+        String fileName2 = args[1];
+        String input;
+
+        ArrayList<String> fileContent = new ArrayList<String>();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName1))) {
+            while ((input = fileReader.readLine()) != null)
+                fileContent.add(input);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+        StringBuffer sbLine = new StringBuffer();
+        for (String line : fileContent) {
+            String[] splitedLine = line.split(" ");
+            for (String word : splitedLine) {
+                if (word.length() > 6)
+
+                    sbLine.append(word).append(" ");
+            }
+        }
+
+        String resultLine = sbLine.toString().trim().replaceAll(" ", ",");
+        try (FileWriter fileWriter = new FileWriter(fileName2)) {
+            fileWriter.write(resultLine);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+    }
+}
